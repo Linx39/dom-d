f5b77e7cb973bb663a2d3a9881a0c8712b2be832
Linx <olga-reshetova@mail.ru>
2025-02-10 13:47:42 +0300

    add interactive style

--- source/js/app.js
+++ source/js/app.js
@@ -2,6 +2,7 @@ import './products-slider.js';
 import './gallery-slider.js';
 import './about-slider.js';
 import './brands-slider.js';
+import './feedback-modal.js';
 import './calculation-modal.js';
 import './site-menu-modal.js';
 import './site-list.js';
---
+++ source/js/feedback-modal.js
@@ -0,0 +1,5 @@
+import { initModal } from "./modal.js";
+
+const feedback = document.querySelector('.feedback');
+
+const feedbackModal = initModal(feedback);
